2bd51f1c42ca18005349307eb56368dad0ca5968
anthonygolapo <golapoanthony25@gmail.com>
2026-07-20 14:17:22 +0800

    Add Vercel readings ingestion route

---
+++ api/v1/readings.ts
@@ -0,0 +1,11 @@
+import type { IncomingMessage, ServerResponse } from "node:http";
+import { buildApp } from "../../src/app.js";
+import { pool } from "../../src/db/postgres.js";
+
+const appPromise = buildApp(pool);
+
+export default async function handler(req: IncomingMessage, res: ServerResponse) {
+  const app = await appPromise;
+  await app.ready();
+  app.server.emit("request", req, res);
+}
